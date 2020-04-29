aa23248a0b5bd86996377885f547dd71d863e412
1169794338@qq.com <Liziyi521521>
2020-04-29 22:21:46 +0800

    ftp存储

---
+++ src/pages/setting/plugin/storagePlugin/components/StandardTable/index.tsx
@@ -0,0 +1,46 @@
+import { Alert, Table } from 'antd';
+import { ColumnProps, TableProps } from 'antd/es/table';
+import React, { Fragment } from 'react';
+
+import { TableListItem } from '../data';
+import styles from './index.less';
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+export interface StandardTableProps<T> extends Omit<TableProps<T>, 'columns'> {
+  columns: StandardTableColumnProps[];
+  data: TableListItem[];
+}
+
+export interface StandardTableColumnProps extends ColumnProps<TableListItem> {
+  needTotal?: boolean;
+  total?: number;
+}
+
+interface StandardTableState {
+  selectedRowKeys: string[];
+  needTotalList: StandardTableColumnProps[];
+}
+
+// @ts-ignore
+const StandardTable: React.FC<StandardTableProps<TableListItem>, StandardTableState> = (props) => {
+  const { data = [], rowKey, ...rest } = props;
+  return (
+    <div className={styles.standardTable}>
+      <div className={styles.tableAlert}>
+        <Alert
+          message={
+            <Fragment>
+              共 <a style={{ fontWeight: 600 }}>{data.length}</a> 项&nbsp;&nbsp;
+            </Fragment>
+          }
+          type="info"
+          showIcon
+        />
+      </div>
+      <Table rowKey={rowKey || 'id'} dataSource={data} pagination={false} {...rest} />
+    </div>
+  );
+};
+
+export default StandardTable;
