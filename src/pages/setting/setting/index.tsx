ff7fed00acb6a8ade9fbcdfa9024a01dedcba37a
1169794338@qq.com <Liziyi521521>
2020-04-29 23:14:16 +0800

    系统设置模块

---
+++ src/pages/setting/setting/index.tsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { connect } from 'dva';
+import { Dispatch, AnyAction } from 'umi';
+import { StateType } from './model';
+
+interface TableListProps {
+  match: {
+    params: {
+      [key: string]: string;
+    };
+  };
+  dispatch: Dispatch<AnyAction>;
+  loading: boolean;
+  setting: StateType;
+}
+
+class TableList extends Component<TableListProps> {
+  componentDidMount() {
+    this.list({});
+  }
+
+  list = (params: {}) => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'setting/edit',
+      payload: params,
+    });
+  };
+
+  render() {
+    const {
+      setting: { data },
+    } = this.props;
+    console.log(data);
+    return <PageHeaderWrapper title={false}>setting</PageHeaderWrapper>;
+  }
+}
+
+export default connect(
+  ({
+    setting,
+    loading,
+  }: {
+    setting: StateType;
+    loading: {
+      effects: {
+        [key: string]: boolean;
+      };
+    };
+  }) => ({
+    setting,
+    loading: loading.effects['setting/edit'] || loading.effects['setting/update'],
+  }),
+)(TableList);
