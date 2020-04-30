448079d5c12a0ba93956d6a529ac3393f4dfcbfa
xial@plian.net <a12345678>
2020-04-30 17:21:07 +0800

    用户管理模块列表

---
+++ src/pages/setting/admin/data.d.ts
@@ -0,0 +1,57 @@
+/**
+ * 列表展示的字段
+ */
+export interface TableListItem {
+  id?: number;
+  isEnabled?: boolean;
+  name?: string;
+  mobile?: string;
+  username?: string;
+  gmtCreate?: Date;
+  lastModifiedDate?: Date;
+  roleNames?: [];
+}
+
+/**
+ * 表单展示的数据
+ */
+export interface AdminForm {
+  id: number;
+  username: string;
+  departmentId: number;
+  name: string;
+  roleIds: [];
+}
+
+/**
+ * 分页数据
+ */
+export interface TableListPagination {
+  total: number;
+  pageSize: number;
+  current: number;
+}
+
+export interface TableListData {
+  list: TableListItem[];
+  pagination: Partial<TableListPagination>;
+}
+
+/**
+ * 搜索的数据
+ */
+export interface TableListParams {
+  sorter: string;
+  status: string;
+  name: string;
+  current: number;
+  currentPage: number;
+  pageNumber: number;
+  orderProperty: string;
+  orderDirection: string;
+}
+
+export interface DepartmentData {
+  id: number;
+  name: string;
+}
