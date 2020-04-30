e0c725e0109bd9e669c45b4aa9c602a8fe0ceacb
xial@plian.net <a12345678>
2020-04-30 17:21:29 +0800

    用户管理模块列表

---
+++ src/pages/setting/admin/service.ts
@@ -0,0 +1,102 @@
+import request from '@/utils/request';
+import { TableListParams } from './data.d';
+import constants from '@/utils/constants';
+
+export async function list(params: TableListParams) {
+  return request(`${constants.baseUrl}/admin/list`, {
+    method: 'POST',
+    data: {
+      ...params,
+    },
+  });
+}
+
+export async function save(params: TableListParams) {
+  return request(`${constants.baseUrl}/save`, {
+    method: 'POST',
+    data: {
+      ...params,
+    },
+  });
+}
+
+export async function edit(params: TableListParams) {
+  return request(`${constants.baseUrl}/admin/edit`, {
+    method: 'POST',
+    data: {
+      ...params,
+    },
+  });
+}
+
+export async function remove(params: TableListParams) {
+  return request(`${constants.baseUrl}/admin/delete`, {
+    method: 'POST',
+    data: {
+      ...params,
+    },
+  });
+}
+
+export async function roleList(params: TableListParams) {
+  return request(`${constants.baseUrl}/role/listAll`, {
+    method: 'POST',
+    data: {
+      ...params,
+    },
+  });
+}
+
+export async function enabled(params: TableListParams) {
+  return request(`${constants.baseUrl}/admin/enabled`, {
+    method: 'POST',
+    data: {
+      ...params,
+    },
+  });
+}
+
+export async function disabled(params: TableListParams) {
+  return request(`${constants.baseUrl}/admin/disabled`, {
+    method: 'POST',
+    data: {
+      ...params,
+    },
+  });
+}
+
+export async function reset(params: TableListParams) {
+  return request(`${constants.baseUrl}/admin/reset`, {
+    method: 'POST',
+    data: {
+      ...params,
+    },
+  });
+}
+
+export async function permission(params: TableListParams) {
+  return request(`${constants.baseUrl}/admin/permission`, {
+    method: 'POST',
+    data: {
+      ...params,
+    },
+  });
+}
+
+export async function checkExport(params: TableListParams) {
+  return request(`${constants.baseUrl}/admin/check_export`, {
+    method: 'POST',
+    data: {
+      ...params,
+    },
+  });
+}
+
+export async function importSave(params: TableListParams) {
+  return request(`${constants.baseUrl}/admin/import_save`, {
+    method: 'POST',
+    data: {
+      ...params,
+    },
+  });
+}
