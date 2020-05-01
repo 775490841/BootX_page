c1a6bad10afc24c12d5a742016251a06625bef43
1169794338@qq.com <Liziyi521521>
2020-05-01 12:13:46 +0800

    调整logo图片和文字

---
+++ src/pages/system/department/data.d.ts
@@ -0,0 +1,16 @@
+/**
+ * 列表展示的字段
+ */
+export interface TableListItem {
+  id?: number;
+  name?: string;
+  order?: number;
+  createdDate?: Date;
+  children: TableListItem[];
+}
+
+export interface DepartmentTree {
+  id: number;
+  name: string;
+  children: DepartmentTree[];
+}
