aed07bf2ab5959d5488f8efaa2d57a7fcaefe820
1169794338@qq.com <Liziyi521521>
2020-05-01 23:24:04 +0800

    字典分类和字典项

---
+++ src/pages/system/word/data.d.ts
@@ -0,0 +1,41 @@
+/**
+ * 列表展示的字段
+ */
+export interface TableListItem {
+  id: number;
+  name: string;
+  memo: string;
+  isEnabled: boolean;
+  isDefault: boolean;
+  departmentName: string;
+  createdDate: Date;
+}
+
+/**
+ * 分页数据
+ */
+export interface TableListPagination {
+  total: number;
+  pageSize: number;
+  current: number;
+}
+
+export interface TableListData {
+  list: TableListItem[];
+  pagination: Partial<TableListPagination>;
+}
+
+/**
+ * 搜索的数据
+ */
+export interface TableListParams {
+  id: null;
+  sorter: string;
+  status: string;
+  name: string;
+  current: number;
+  currentPage: number;
+  pageNumber: number;
+  orderProperty: string;
+  orderDirection: string;
+}
