f1aa1920807a0a645cdae8c7f86743d014bee781
1169794338@qq.com <Liziyi521521>
2020-05-01 12:42:04 +0800

    调整logo图片和文字

--- src/pages/system/admin/service.ts
+++ src/pages/system/admin/service.ts
@@ -1,6 +1,6 @@
 import request from '@/utils/request';
-import { TableListParams } from './data.d';
 import constants from '@/utils/constants';
+import { TableListParams } from './data.d';
 
 export async function list(params: TableListParams) {
   return request(`${constants.baseUrl}/admin/list`, {
