7690e1ae2ce7d5012d14ef7f2fcd618ff329bb2b
1169794338@qq.com <Liziyi521521>
2020-05-01 12:13:59 +0800

    调整logo图片和文字

---
+++ src/pages/system/department/model.ts
@@ -0,0 +1,70 @@
+import { AnyAction, Reducer } from 'umi';
+import { EffectsCommandMap } from 'dva';
+import { list, save, tree } from './service';
+
+import { TableListItem } from './data.d';
+
+export interface StateType {
+  data: TableListItem[];
+  value?: TableListItem;
+}
+
+export type Effect = (
+  action: AnyAction,
+  effects: EffectsCommandMap & { select: <T>(func: (state: StateType) => T) => T },
+) => void;
+
+export interface ModelType {
+  namespace: string;
+  state: StateType;
+  effects: {
+    list: Effect;
+    save: Effect;
+    tree: Effect;
+  };
+  reducers: {
+    listInfo: Reducer<StateType>;
+    editInfo: Reducer<StateType>;
+  };
+}
+
+const Model: ModelType = {
+  namespace: 'department',
+
+  state: {
+    data: [],
+  },
+
+  effects: {
+    *list({ payload }, { call, put }) {
+      const response = yield call(list, payload);
+      yield put({
+        type: 'listInfo',
+        payload: response,
+      });
+    },
+    *save({ payload, callback }, { call }) {
+      const response = yield call(save, payload);
+      if (callback) {
+        callback(response);
+      }
+    },
+    *tree({ payload, callback }, { call }) {
+      const response = yield call(tree, payload);
+      if (callback) {
+        callback(response);
+      }
+    },
+  },
+
+  reducers: {
+    listInfo(state, action) {
+      return {
+        ...state,
+        data: action.payload || [],
+      };
+    },
+  },
+};
+
+export default Model;
