c74710b79be9bd6bd9c8a73076fe1fd677be291d
1169794338@qq.com <Liziyi521521>
2020-05-01 12:40:54 +0800

    调整logo图片和文字

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -12,7 +12,6 @@ import ProLayout, {
 } from '@ant-design/pro-layout';
 import React, { useEffect } from 'react';
 import { Link, useIntl, connect, Dispatch } from 'umi';
-import { GithubOutlined } from '@ant-design/icons';
 import { Result, Button } from 'antd';
 import Authorized from '@/utils/Authorized';
 import RightContent from '@/components/GlobalHeader/RightContent';
@@ -24,10 +23,10 @@ const noMatch = (
   <Result
     status={403}
     title="403"
-    subTitle="Sorry, you are not authorized to access this page."
+    subTitle="您无权访问该页面"
     extra={
       <Button type="primary">
-        <Link to="/user/login">Go Login</Link>
+        <Link to="/user/login">返回登陆</Link>
       </Button>
     }
   />
@@ -57,31 +56,7 @@ const menuDataRender = (menuList: MenuDataItem[]): MenuDataItem[] =>
     return Authorized.check(item.authority, localItem, null) as MenuDataItem;
   });
 
-const defaultFooterDom = (
-  <DefaultFooter
-    copyright="2019 蚂蚁金服体验技术部出品"
-    links={[
-      {
-        key: 'Ant Design Pro',
-        title: 'Ant Design Pro',
-        href: 'https://pro.ant.design',
-        blankTarget: true,
-      },
-      {
-        key: 'github',
-        title: <GithubOutlined />,
-        href: 'https://github.com/ant-design/ant-design-pro',
-        blankTarget: true,
-      },
-      {
-        key: 'Ant Design',
-        title: 'Ant Design',
-        href: 'https://ant.design',
-        blankTarget: true,
-      },
-    ]}
-  />
-);
+const defaultFooterDom = <DefaultFooter copyright="2020 BootX 开发平台" links={false} />;
 
 const BasicLayout: React.FC<BasicLayoutProps> = (props) => {
   const {
@@ -125,10 +100,10 @@ const BasicLayout: React.FC<BasicLayoutProps> = (props) => {
       <ProLayout
         logo={logo}
         formatMessage={formatMessage}
-        menuHeaderRender={(logoDom, titleDom) => (
+        menuHeaderRender={(logoDom) => (
           <Link to="/">
             {logoDom}
-            {titleDom}
+            <h1>开发平台</h1>
           </Link>
         )}
         onCollapse={handleMenuCollapse}
