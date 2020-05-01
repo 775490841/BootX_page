0fce3ccebddc07c6f67aeb685b657ebd89a37cbd
1169794338@qq.com <Liziyi521521>
2020-05-01 12:41:09 +0800

    调整logo图片和文字

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -50,10 +50,10 @@ const UserLayout: React.FC<UserLayoutProps> = (props) => {
             <div className={styles.header}>
               <Link to="/">
                 <img alt="logo" className={styles.logo} src={logo} />
-                <span className={styles.title}>Ant Design</span>
+                <span className={styles.title}>快速开发平台</span>
               </Link>
             </div>
-            <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
+            <div className={styles.desc}>BootX 快速开发平台</div>
           </div>
           {children}
         </div>
