b18a9013f0811facfd5c5d0ad3eabaa660859648
1169794338@qq.com <Liziyi521521>
2020-05-01 23:23:46 +0800

    字典分类和字典项

--- config/config.ts
+++ config/config.ts
@@ -166,6 +166,50 @@ export default defineConfig({
                     },
                   ],
                 },
+                {
+                  name: 'word',
+                  path: '/system/word',
+                  routes: [
+                    {
+                      path: '/system/word',
+                      component: './system/word',
+                    },
+                    {
+                      path: '/system/word/add',
+                      name: 'add',
+                      hideInMenu: true,
+                      component: './system/word/add',
+                    },
+                    {
+                      path: '/system/word/edit/:id',
+                      name: 'edit',
+                      hideInMenu: true,
+                      component: './system/word/add',
+                    },
+                  ],
+                },
+                {
+                  name: 'wordType',
+                  path: '/system/wordType',
+                  routes: [
+                    {
+                      path: '/system/wordType',
+                      component: './system/wordType',
+                    },
+                    {
+                      path: '/system/wordType/add',
+                      name: 'add',
+                      hideInMenu: true,
+                      component: './system/wordType/add',
+                    },
+                    {
+                      path: '/system/wordType/edit/:id',
+                      name: 'edit',
+                      hideInMenu: true,
+                      component: './system/wordType/add',
+                    },
+                  ],
+                },
               ],
             },
             {
