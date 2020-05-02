fd7cf982b067f7765cff2ac101bed39226d1cc66
1169794338@qq.com <Liziyi521521>
2020-05-02 21:35:46 +0800

    权限控制

---
+++ src/pages/system/permission/model.ts
@@ -0,0 +1,120 @@
+import { AnyAction, Reducer } from 'umi';
+import { EffectsCommandMap } from 'dva';
+import { list, edit, save, remove, disabled, enabled, menuTree } from './service';
+
+import { TableListData, TableListItem } from './data.d';
+
+export interface StateType {
+  data?: TableListData;
+  value?: TableListItem;
+}
+
+export type Effect = (
+  action: AnyAction,
+  effects: EffectsCommandMap & { select: <T>(func: (state: StateType) => T) => T },
+) => void;
+
+export interface ModelType {
+  namespace: string;
+  state: StateType;
+  effects: {
+    list: Effect;
+    save: Effect;
+    remove: Effect;
+    edit: Effect;
+    disabled: Effect;
+    enabled: Effect;
+    menuTree: Effect;
+  };
+  reducers: {
+    listInfo: Reducer<StateType>;
+    editInfo: Reducer<StateType>;
+  };
+}
+
+const Model: ModelType = {
+  namespace: 'permission',
+
+  state: {
+    data: {
+      list: [],
+      pagination: {},
+    },
+    values: {},
+  },
+
+  effects: {
+    *list({ payload }, { call, put }) {
+      const response = yield call(list, payload);
+      yield put({
+        type: 'listInfo',
+        payload: response,
+      });
+    },
+    *save({ payload, callback }, { call }) {
+      const response = yield call(save, payload);
+      if (callback) {
+        callback(response);
+      }
+    },
+    *remove({ payload, callback }, { call }) {
+      const response = yield call(remove, payload);
+      if (callback) {
+        callback(response);
+      }
+    },
+    *edit({ payload, callback }, { call, put }) {
+      const response = yield call(edit, payload);
+      yield put({
+        type: 'editInfo',
+        payload: response,
+      });
+      if (callback) {
+        callback(response);
+      }
+    },
+    *enabled({ payload, callback }, { call }) {
+      const response = yield call(enabled, payload);
+      if (callback) {
+        callback(response);
+      }
+    },
+    *disabled({ payload, callback }, { call }) {
+      const response = yield call(disabled, payload);
+      if (callback) {
+        callback(response);
+      }
+    },
+    *menuTree({ payload, callback }, { call }) {
+      const response = yield call(menuTree, payload);
+      if (callback) {
+        callback(response);
+      }
+    },
+  },
+
+  reducers: {
+    listInfo(state, action) {
+      const { data = [], total = 0, current = 1, pageSize = 0 } = action.payload;
+      return {
+        ...state,
+        data: {
+          list: data,
+          pagination: {
+            current,
+            pageSize,
+            total,
+          },
+        },
+      };
+    },
+    editInfo(state, action) {
+      return {
+        ...state,
+        values: action.payload,
+      };
+    },
+  },
+};
+
+export default Model;
