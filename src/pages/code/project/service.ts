e5c0a316e5425afa8c778d0ecd63978ff8d60c58
1169794338@qq.com <Liziyi521521>
2020-05-07 23:57:26 +0800

    账号退出的逻辑

--- src/pages/code/project/service.ts
+++ src/pages/code/project/service.ts
@@ -3,7 +3,7 @@ import constants from '@/utils/constants';
 import { TableListParams } from './data.d';
 
 export async function list(params: TableListParams) {
-  return request(`${constants.baseUrl}/admin/list`, {
+  return request(`${constants.projectUrl}/project/list`, {
     method: 'POST',
     data: {
       ...params,
@@ -13,14 +13,14 @@ export async function list(params: TableListParams) {
 
 export async function save(params: TableListParams) {
   if (params.id) {
-    return request(`${constants.baseUrl}/admin/update`, {
+    return request(`${constants.projectUrl}/project/update`, {
       method: 'POST',
       data: {
         ...params,
       },
     });
   }
-  return request(`${constants.baseUrl}/admin/save`, {
+  return request(`${constants.projectUrl}/project/save`, {
     method: 'POST',
     data: {
       ...params,
@@ -29,7 +29,7 @@ export async function save(params: TableListParams) {
 }
 
 export async function edit(params: TableListParams) {
-  return request(`${constants.baseUrl}/admin/edit`, {
+  return request(`${constants.projectUrl}/project/edit`, {
     method: 'POST',
     data: {
       ...params,
@@ -38,88 +38,7 @@ export async function edit(params: TableListParams) {
 }
 
 export async function remove(params: TableListParams) {
-  return request(`${constants.baseUrl}/admin/delete`, {
-    method: 'POST',
-    data: {
-      ...params,
-    },
-  });
-}
-
-export async function roleList(params: TableListParams) {
-  return request(`${constants.baseUrl}/role/all`, {
-    method: 'POST',
-    data: {
-      ...params,
-    },
-  });
-}
-
-export async function enabled(params: TableListParams) {
-  return request(`${constants.baseUrl}/admin/enabled`, {
-    method: 'POST',
-    data: {
-      ...params,
-    },
-  });
-}
-
-export async function disabled(params: TableListParams) {
-  return request(`${constants.baseUrl}/admin/disabled`, {
-    method: 'POST',
-    data: {
-      ...params,
-    },
-  });
-}
-
-export async function reset(params: TableListParams) {
-  return request(`${constants.baseUrl}/admin/reset`, {
-    method: 'POST',
-    data: {
-      ...params,
-    },
-  });
-}
-
-export async function permission(params: TableListParams) {
-  return request(`${constants.baseUrl}/admin/permission`, {
-    method: 'POST',
-    data: {
-      ...params,
-    },
-  });
-}
-
-export async function checkExport(params: TableListParams) {
-  return request(`${constants.baseUrl}/admin/check_export`, {
-    method: 'POST',
-    data: {
-      ...params,
-    },
-  });
-}
-
-export async function importSave(params: TableListParams) {
-  return request(`${constants.baseUrl}/admin/import_save`, {
-    method: 'POST',
-    data: {
-      ...params,
-    },
-  });
-}
-
-export async function departmentTree(params: TableListParams) {
-  return request(`${constants.baseUrl}/department/tree`, {
-    method: 'POST',
-    data: {
-      ...params,
-    },
-  });
-}
-
-export async function add(params: TableListParams) {
-  return request(`${constants.baseUrl}/admin/add`, {
+  return request(`${constants.projectUrl}/project/delete`, {
     method: 'POST',
     data: {
       ...params,
